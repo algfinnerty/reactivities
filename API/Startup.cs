b04b91e27dede4500ae8bbb27e38e66571803f5e
Annie Finnerty <afinnerty@webitservices.com>
2021-11-05 17:28:13 -0600

    end section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,6 +38,13 @@ namespace API
             services.AddDbContext<DataContext>(opt =>
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"))
             );
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,8 +58,10 @@ namespace API
             }
 
             //app.UseHttpsRedirection();
-
+            //the ordering is important
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
