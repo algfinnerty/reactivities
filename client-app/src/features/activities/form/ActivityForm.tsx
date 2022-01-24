e16e1658e0987f145ddb76f71bea1c43af7a939e
Annie Finnerty <afinnerty@webitservices.com>
2022-01-24 15:22:10 -0700

    End of section 6

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -6,9 +6,10 @@ interface Props{
     activity: Activity| undefined;
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
+    submitting: boolean
 }
 
-export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit} : Props){
+export default function ActivityForm({activity: selectedActivity, closeForm, createOrEdit, submitting} : Props){
     
     const initialState = selectedActivity ?? {
         id: '',
@@ -36,12 +37,12 @@ export default function ActivityForm({activity: selectedActivity, closeForm, cre
         <Segment clearing>
             <Form onSubmit={handleSubmit} autoComplete="off">
                 <Form.Input placeholder='Title' value={activity.title} name='title' onChange={handleInputChange}/>
-                <Form.Input placeholder='Description' value={activity.description} name='title' onChange={handleInputChange}/>
-                <Form.Input placeholder='Category' value={activity.category} name='title' onChange={handleInputChange}/>
-                <Form.Input placeholder='Date' value={activity.date} name='title' onChange={handleInputChange}/>
-                <Form.Input placeholder='City' value={activity.city} name='title' onChange={handleInputChange}/>
-                <Form.Input placeholder='Venue' value={activity.venue} name='title' onChange={handleInputChange}/>
-                <Button floated='right' positive type='submit' content='Submit'/>
+                <Form.Input placeholder='Description' value={activity.description} name='description' onChange={handleInputChange}/>
+                <Form.Input placeholder='Category' value={activity.category} name='category' onChange={handleInputChange}/>
+                <Form.Input type="date" placeholder='Date' value={activity.date} name='date' onChange={handleInputChange}/>
+                <Form.Input placeholder='City' value={activity.city} name='city' onChange={handleInputChange}/>
+                <Form.Input placeholder='Venue' value={activity.venue} name='venue' onChange={handleInputChange}/>
+                <Button floated='right' positive type='submit' content='Submit' loading={submitting}/>
                 <Button onClick={closeForm} floated='right' type='button' content='Cancel'/>
             </Form>
         </Segment>
